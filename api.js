7cc8a9e55f8be89e61e46ff100437f386ce51d96
Jeffrey <Jmmercer@ucalgary.ca>
2017-03-02 21:48:35 +0000

    validator and added some es6 syntax

--- api.js
+++ api.js
@@ -1,69 +1,76 @@
-var rest = require('restler');
+const isValidDate = require('./validDate');
+const rest = require('restler');
 
-var self = module.exports = {
+const self = module.exports = {
 
-	ver001: (data, res) => {
+    ver002: (data, res) => {
 
-		if (typeof data.base === 'undefined' || data.base === '') {
-			self.sendResponse(res, 403, 'Please supply a base currency symbol');
-			return;
-		}
+        if (typeof data.base === 'undefined' || data.base === '') {
+            self.sendResponse(res, 403, 'Please supply a base currency symbol');
+            return;
+        }
 
-		var base = data.base.toUpperCase();
+         base = data.base.toUpperCase();
 
-		var url = 'https://api.fixer.io/latest?symbols=' + data.symbol.from + ',' + data.symbol.to;
+        const url = 'https://api.fixer.io/latest?symbols=' + data.symbol.from + ',' + data.symbol.to;
 
-		if (typeof data.symbol === 'undefined' || data.symbol === '') {
-			self.sendResponse(res, 403, 'Please supply a currency symbol to convert to');
-			return;
-		}
+        if (typeof data.symbol === 'undefined' || data.symbol === '') {
+            self.sendResponse(res, 403, 'Please supply a currency symbol to convert to');
+            return;
+        }
 
-		if (typeof data.amount === 'undefined' || data.amount === '') {
-			self.sendResponse(res, 403, 'Please supply an amount to convert');
-			return;
-		}
+        if (typeof data.amount === 'undefined' || data.amount === '') {
+            self.sendResponse(res, 403, 'Please supply an amount to convert');
+            return;
+        }
 
-		if (typeof data.symbol === 'object') {
+        if (typeof data.symbol === 'object') {
 
-			var str = '';
-			var symbolArray = data.symbol;
+            let str = '';
+            var symbolArray = data.symbol;
 
-			for (var i = symbolArray.length - 1; i >= 0; i--) {
-				str += symbolArray[i].toUpperCase() + ',';
-			}
+            for (let i = symbolArray.length - 1; i >= 0; i--) {
+                str += symbolArray[i].toUpperCase() + ',';
+            }
 
-			var symbols = str;
+            const symbols = str;
 
-		} else {
+        } else {
 
-			var symbols = data.symbol.toUpperCase();
+            const symbols = data.symbol.toUpperCase();
 
-		}
+        }
 
-		if (typeof data.date !== 'undefined') {
-			if (typeof data.date !== 'string') {
-				self.sendResponse(res, 403, 'Please provide the date as a string');
-				return;
-			}
-			var date = data.date;
-		} else {
-			var date = 'latest';
-		}
+        if (typeof data.date !== 'undefined') {
+            if (typeof data.date !== 'string') {
+                self.sendResponse(res, 403, 'Please provide the date as a string');
+                return;
+            }
+            // Validate Date
+            if (isValidDate(data.date)) {
+                var date = data.date
+            } else {
+                self.sendResponse(res, 403, 'Please provide a valid date in YY-MM-DD format');
+                return;
+            }
+        } else {
+            let date = 'latest';
+        }
 
-		var url = 'http://api.fixer.io/' + date + '?base=' + base + '&symbols=' + symbols;
+        const url = 'http://api.fixer.io/' + date + '?base=' + base + '&symbols=' + symbols;
 
         rest.get(url).on('complete', function(err, response) {
 
             if (response.statusCode == 200) {
 
-            	var returns = {
-            		base: data.base,
-            		amount: data.amount,
-            		results: self.convertAmount(data.amount, JSON.parse(response.rawEncoded)),
-            		dated: data.date
-            	};
+                let returns = {
+                    base: data.base,
+                    amount: data.amount,
+                    results: self.convertAmount(data.amount, JSON.parse(response.rawEncoded)),
+                    dated: data.date
+                };
 
-            	self.sendResponse(res, 200, returns);
+                self.sendResponse(res, 200, returns);
             }
             if (response.statusCode == 401) {
                 callback('Not Authorized');
@@ -74,36 +81,36 @@ var self = module.exports = {
 
         });
 
-	},
+    },
 
-	convertAmount: (amount, data) => {
+    convertAmount: (amount, data) => {
 
-		var rates = data.rates;
-		var returns = [];
+        const rates = data.rates;
+        let returns = [];
 
-		for (var r in rates) {
+        for (let r in rates) {
 
-			if (rates.hasOwnProperty(r)) {
+            if (rates.hasOwnProperty(r)) {
 
-				var convert = (amount * rates[r]);
-				returns.push({from: data.base, to: r, roundedResult: convert.toFixed(2), fullResult: convert, rate: rates[r]})
+                let convert = (amount * rates[r]);
+                returns.push({from: data.base, to: r, roundedResult: convert.toFixed(2), fullResult: convert, rate: rates[r]})
 
-			}
+            }
 
-		}
+        }
 
-		return returns;
-	},
+        return returns;
+    },
 
-	sendResponse: (res, status, response) => {
+    sendResponse: (res, status, response) => {
 
-        if(typeof response === 'object'){
+        if (typeof response === 'object') {
             response = JSON.stringify(response);
         }
-	    res.status(status);
-	    res.write(response);
-	    res.end();
-	    return
+        res.status(status);
+        res.write(response);
+        res.end();
+        return;
 
-	}
+    }
 }
